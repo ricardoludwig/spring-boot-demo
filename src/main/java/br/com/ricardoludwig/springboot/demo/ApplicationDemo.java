bf50635f628b8196f13cd782f5ed001b30676330
Ricardo Ludwig <ricardo.ludwig@gmail.com>
2018-07-24 23:00:35 -0300

    Enabled Eureka - Ribbon Client Side Load Balancing - Show own register instance at Eureka

--- src/main/java/br/com/ricardoludwig/springboot/demo/ApplicationDemo.java
+++ src/main/java/br/com/ricardoludwig/springboot/demo/ApplicationDemo.java
@@ -1,10 +1,14 @@
 package br.com.ricardoludwig.springboot.demo;
 
 import java.util.Date;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,20 +19,37 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class ApplicationDemo {
 
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ApplicationDemo.class).web(WebApplicationType.SERVLET).run(args);
+	}
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	/**
+	 * Show own register instance at Eureka
+	 * @param applicationName
+	 * @return
+	 */
+	@RequestMapping("/show-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
+
 	@RequestMapping(value = "/show/{name}")
 	public String echoStudentName(@PathVariable(name = "name") String name) {
 		return "Hello  " + name + " Responsed on : " + new Date();
 	}
 
-	@RequestMapping(value = "/showDetails/{name}")
+	@RequestMapping(value = "/showAge/{name}")
 	public Person getStudentDetails(@PathVariable(name = "name") String name) {
 		return new Person(name, 40);
 	}
 
-	public static void main(String[] args) {
-//		SpringApplication.run(ApplicationDemo.class, args);
-		new SpringApplicationBuilder(ApplicationDemo.class).web(WebApplicationType.SERVLET).run(args);
-	}
 }
 
 class Person {
